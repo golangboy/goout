d3fb499c7ec8fa85cb7dda49f2a185fc68f3e7f0
GolangBoy <golangboy@qq.com>
2022-10-28 13:15:49 +0800

    more features

--- journal.go
+++ journal.go
@@ -23,8 +23,16 @@ func LogInfo(v any) {
 
 func LogError(v any) {
 	_, file, line, _ := runtime.Caller(1)
+	_, file2, line2, _ := runtime.Caller(2)
+	_, file3, line3, _ := runtime.Caller(3)
+	_, file4, line4, _ := runtime.Caller(4)
+	_, file5, line5, _ := runtime.Caller(5)
 	log.Print("[LogError]")
 	log.Print(file + ":" + strconv.Itoa(line))
+	log.Println(file2 + ":" + strconv.Itoa(line2))
+	log.Println(file3 + ":" + strconv.Itoa(line3))
+	log.Println(file4 + ":" + strconv.Itoa(line4))
+	log.Println(file5 + ":" + strconv.Itoa(line5))
 	log.Println(v)
 	log.Println()
 	log.Println()
